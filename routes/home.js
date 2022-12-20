552c3eeab5725a37c3a2c31ddc06ac4b82d27b69
DamyanBG <d.dimitrov.1988@abv.bg>
2022-12-20 23:35:40 +0200

    refactor home crud and upload photo to azure blob

--- routes/home.js
+++ routes/home.js
@@ -1,76 +1,82 @@
 const router = require("express").Router();
-const Homes = require("../models/Homes");
-const { createHome, getHomeById, deleteHome, updateHome } = require("../services/homeService");
+const { isValidObjectId } = require("mongoose");
+const {
+  createHome,
+  getHomeById,
+  deleteHome,
+  updateHome,
+} = require("../services/homeService");
 
 //Create home
 router.post("/", async (req, res) => {
-    const home = {
-      name: req.body.name,
-      place: req.body.place,
-      price: Number(req.body.price),
-      size: req.body.size,
-      year: req.body.year,
-      description: req.body.description,
-    };
-  
-    try {
-      const newHome = await createHome(home);
-      return res.status(201).json(newHome);
-    } catch (err) {
-      return res.status(500).json(err);
-    }
-  });
+  const home = {
+    name: req.body.name,
+    place: req.body.place,
+    price: Number(req.body.price),
+    size: req.body.size,
+    year: req.body.year,
+    description: req.body.description,
+  };
+
+  const newHome = await createHome(home);
+  return res.status(201).json(newHome);
+});
 
 //Get home
 router.get("/", async (req, res) => {
-    if (req.body.home_id) {
-        try {
-            const home = await getHomeById(req.body.home_id);
-            const { password, updatedAt,createdAt, ...other } = home._doc;
-            res.status(200).json(other);
-          } catch (err) {
-            return res.status(500).json(err);
-          }
-    }else{
-        return res.status(500).json("Home with this id is dosen't existing");
-    }    
-  });
+  const homeId = req.body.home_id;
+  if (!isValidObjectId(homeId)) {
+    res.status(400).json("Invalid home id!");
+    return;
+  }
 
-  //Delete home
-router.delete("/", async (req, res) => {
-    if (req.body.home_id) {
-        try {
-            await deleteHome(req.body.home_id);
-            res.status(200).json("Home property is delete");
-          } catch (err) {
-            return res.status(500).json(err);
-          }
-    }else{
-        return res.status(500).json("You can delete not existing home");
-    }
-  });
-
-  //Update home
-router.put("/", async (req, res) => {
-    const homeId = req.body.home_id;
-    const home = {       
-            name: req.body.name,
-            place: req.body.place,
-            price: Number(req.body.price),
-            time: req.body.time,
-            size: req.body.size,
-            year: req.body.year,
-            description: req.body.description,    
-    }
-
-
-    try {
-      const currentHome = await updateHome(homeId, home);
-      return res.status(200).json(currentHome);
-    } catch (err) {
-      return res.status(500).json(err);
-    }
+  const home = await getHomeById(req.body.home_id);
+  if (!home) {
+    res.status(404).send("Home with this id do not exists");
+    return;
+  }
+  res.status(200).json(home);
+  return;
 });
 
+//Delete home
+router.delete("/", async (req, res) => {
+  const homeId = req.body.home_id;
+  if (!isValidObjectId(homeId)) {
+    res.status(400).json("Invalid home id!");
+    return;
+  }
+  try {
+    await deleteHome(req.body.home_id);
+    res.status(200).json("Home property is delete");
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+//Update home
+router.put("/", async (req, res) => {
+  const homeId = req.body.home_id;
+  if (!isValidObjectId(homeId)) {
+    res.status(400).json("Invalid home id!");
+    return;
+  }
+  const home = {
+    name: req.body.name,
+    place: req.body.place,
+    price: Number(req.body.price),
+    time: req.body.time,
+    size: req.body.size,
+    year: req.body.year,
+    description: req.body.description,
+  };
+
+  try {
+    const currentHome = await updateHome(homeId, home);
+    return res.status(200).json(currentHome);
+  } catch (err) {
+    return res.status(404).json(err);
+  }
+});
 
 module.exports = router;
--- routes/homes.js
+++ routes/homes.js
@@ -1,16 +1,10 @@
 const router = require("express").Router();
-const Homes = require("../models/Homes");
 const { getAllHomes } = require("../services/homeService");
 
 //Get all homes
-router.get("/", async (req,res) => {
-    try {
-        const homes = await getAllHomes();
-        res.status(200).json(homes);
-    } catch (err) {
-        return res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  const homes = await getAllHomes();
+  res.status(200).json(homes);
 });
-
 
 module.exports = router;
