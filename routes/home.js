cb673dd458299070b7e0520a5bfac87c51c11fd4
DamyanBG <d.dimitrov.1988@abv.bg>
2022-12-27 10:57:03 +0200

    add lon & lat

--- routes/home.js
+++ routes/home.js
@@ -1,105 +1,96 @@
-const router = require("express").Router();
-const { isValidObjectId } = require("mongoose");
-const {
-  createHome,
-  getHomeById,
-  deleteHome,
-  updateHome,
-} = require("../services/homeService");
-const { body, validationResult } = require("express-validator");
+const router = require('express').Router();
+const { isValidObjectId } = require('mongoose');
+const { createHome, getHomeById, deleteHome, updateHome } = require('../services/homeService');
+const { body, validationResult } = require('express-validator');
+
+const reqBodyToObject = (req) => ({
+  name: req.body.name,
+  place: req.body.place,
+  price: Number(req.body.price),
+  size: req.body.size,
+  year: req.body.year,
+  description: req.body.description,
+  longitude: req.body.longitude,
+  latitude: req.body.latitude
+});
 
 //Create home
 router.post(
-  "/",
-  body("name")
+  '/',
+  body('name')
     .isLength({ min: 3 })
-    .withMessage("Property name must be at least 3 charaters long")
+    .withMessage('Property name must be at least 3 charaters long')
     .isLength({ max: 100 })
-    .withMessage("Property name must be less 10 charaters long"),
-  body("place")
+    .withMessage('Property name must be less 10 charaters long'),
+  body('place')
     .isLength({ min: 3 })
-    .withMessage("Place name must be at least 3 charaters long")
+    .withMessage('Place name must be at least 3 charaters long')
     .isLength({ max: 100 })
-    .withMessage("Place name must be less 12 charaters long"),
-  body("description")
+    .withMessage('Place name must be less 12 charaters long'),
+  body('description')
     .isLength({ min: 5 })
     .withMessage(`Description must be at least 5 character long`)
     .isLength({ max: 100 })
     .withMessage(`Description must less 50 character long`),
   async (req, res) => {
-    const home = {
-      name: req.body.name,
-      place: req.body.place,
-      price: Number(req.body.price),
-      size: req.body.size,
-      year: req.body.year,
-      description: req.body.description,
-    };
+    const home = reqBodyToObject(req)
 
-    try {
-      const { errors } = validationResult(req);
-      if (errors.length > 0) {
-        throw errors;
-      }
-
-      const newHome = await createHome(home);
-      return res.status(201).json(newHome);
-    } catch (err) {
-      console.error(err.message);
-      console.log(err)
-      res.status(400).json({ error: err });
+    const { errors } = validationResult(req);
+    if (errors.length > 0) {
+      res.status(400).json(errors);
+      return;
     }
+
+    const newHome = await createHome(home);
+    return res.status(201).json(newHome);
   }
 );
 
 //Get home
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const homeId = req.params.home_id;
   if (!isValidObjectId(homeId)) {
-    res.status(400).json("Invalid home id!");
+    res.status(400).json('Invalid home id!');
     return;
   }
 
   const home = await getHomeById(homeId);
-  
+
   if (!home) {
-    res.status(404).send("Home with this id do not exists");
+    res.status(404).send('Home with this id do not exists');
     return;
   }
-  
+
   res.status(200).json(home);
   return;
 });
 
 //Delete home
-router.delete("/", async (req, res) => {
+router.delete('/', async (req, res) => {
   const homeId = req.body.home_id;
   if (!isValidObjectId(homeId)) {
-    res.status(400).json("Invalid home id!");
+    res.status(400).json('Invalid home id!');
     return;
   }
-  try {
-    await deleteHome(req.body.home_id);
-    res.status(200).json("Home property is delete");
-  } catch (err) {
-    return res.status(400).json(err);
-  }
+  
+  await deleteHome(req.body.home_id);
+  res.status(200).json('Home property is delete');
 });
 
 //Update home
 router.put(
-  "/",
-  body("name")
+  '/',
+  body('name')
     .isLength({ min: 3 })
-    .withMessage("Property name must be at least 3 charaters long")
+    .withMessage('Property name must be at least 3 charaters long')
     .isLength({ max: 10 })
-    .withMessage("Property name must be less 10 charaters long"),
-  body("place")
+    .withMessage('Property name must be less 10 charaters long'),
+  body('place')
     .isLength({ min: 3 })
-    .withMessage("Place name must be at least 3 charaters long")
+    .withMessage('Place name must be at least 3 charaters long')
     .isLength({ max: 12 })
-    .withMessage("Place name must be less 12 charaters long"),
-  body("description")
+    .withMessage('Place name must be less 12 charaters long'),
+  body('description')
     .isLength({ min: 5 })
     .withMessage(`Description must be at least 5 character long`)
     .isLength({ max: 50 })
@@ -107,30 +98,17 @@ router.put(
   async (req, res) => {
     const homeId = req.body.home_id;
     if (!isValidObjectId(homeId)) {
-      res.status(400).json("Invalid home id!");
+      res.status(400).json('Invalid home id!');
       return;
     }
-    const home = {
-      name: req.body.name,
-      place: req.body.place,
-      price: Number(req.body.price),
-      time: req.body.time,
-      size: req.body.size,
-      year: req.body.year,
-      description: req.body.description,
-    };
-
-    try {
-      const { errors } = validationResult(req);
-      if (errors.length > 0) {
-        throw errors;
-      }
-
-      const currentHome = await updateHome(homeId, home);
-      return res.status(200).json(currentHome);
-    } catch (err) {
-      return res.status(404).json(err);
+    const home = reqBodyToObject(req)
+    const { errors } = validationResult(req);
+    if (errors.length > 0) {
+      res.status(400).json(errors);
+      return;
     }
+    const currentHome = await updateHome(homeId, home);
+    return res.status(200).json(currentHome);
   }
 );
 
